304a837fd594c15684ad4099c81aaf0ca6f30b76
akashmony01 <akashmonya88@gmail.com>
2020-10-14 11:23:03 +0600

    Latest update withsecond page

---
+++ src/script/custom/unique/chooseOs.js
@@ -0,0 +1,120 @@
+// -----------------------------------------------
+// select vesrion script
+$(document).ready(function(){
+    $(".versionName").click(function(){
+        // get the value of version
+        var versionName = $(this).html();
+        // get the current siutable item parent
+        var currentDistro = $(this).parents('.distroFooter');
+        var distroVersion = $(currentDistro).children('.distroVersion');
+        
+        // set choosen version
+        $('.choosenVersion').html("select version");
+        $(distroVersion).children('.choosenVersion').html(versionName);
+    });
+});
+
+// -----------------------------------------------
+// plan tab script
+$(document).ready(function(){
+	$(".cosTabBtn").click(function(){
+		var cosTab = $(this).attr('cosTabId');
+		
+		$(".cosTabBtn").parent().removeClass("active");
+		$(this).parent().addClass("active");
+
+		$(".cosTabItem").removeClass("active");
+		$("#"+cosTab).addClass("active");
+	});
+});
+
+// -----------------------------------------------
+// distro content item script
+$(document).ready(function(){
+    // filesystem show hide function
+	function sshAlertShowHide() {
+        var currentActiveDistro = $(".distroItem.active");
+
+        if ( $( currentActiveDistro ).hasClass( "sshRequired" ) ) {
+            $(".sshAlert").show();
+		}else{
+			$(".sshAlert").hide();
+		}
+    }
+    
+	$(".distroItem").click(function(){
+        // remove active from siblings
+		$('.distroItem').removeClass("active");
+		// add active to new
+        $(this).addClass("active");
+        
+        // ssh alert show hide
+        sshAlertShowHide();
+    });
+    
+    // ssh alert show hide
+    sshAlertShowHide();
+});
+
+// ------------------------------------------------
+// on focus show hide suggestions
+$(document).ready(function() {
+    // show suggestion
+    $( "#searchMarketPlace" ).focusin(function() {
+        $( this ).addClass("active");
+        $( ".searchItemContainer" ).addClass("active");
+    });
+    // remove suggestion
+    $(document).click (function (e) {
+        if (e.target != $('#searchMarketPlace')[0]) {
+            $( this ).removeClass("active");
+            $( ".searchItemContainer" ).removeClass("active");
+        }
+    });
+});
+
+
+// ------------------------------------------------
+// select choosen application item from recommended
+$(document).ready(function() {
+    $(".applicationItem").click(function(){
+        // remove active from siblings
+        $(".applicationItem").removeClass("active");
+        // add active to new
+        $(this).addClass("active");
+    });
+});
+
+// ------------------------------------------------
+// select choosen application item from search
+$(document).ready(function() {
+    $( ".searchItemContainer > .applicationItem" ).click(function() {
+        var choosenApplicationData = $( this ).html()
+        var choosenItem = $(".choosenApplication > .choosenAppContainer > .applicationItem");
+        
+        $(".recommendedApplication").hide();
+        $(".applicationItem").removeClass("active");
+        
+        $(choosenItem).html(choosenApplicationData);
+        $(choosenItem).addClass("active");
+        $(".choosenApplication").addClass("active");
+    });
+});
+
+
+// ------------------------------------------------
+// import url modal
+$(document).ready(function() {
+    // show modal
+    $(".importUrlBtn").click(function(){
+        $(".wrapperForImportUrlModal").addClass("active");
+    });
+    // hide modal
+    $(".cancelUrl").click(function(){
+        $(".wrapperForImportUrlModal").removeClass("active");
+        $("#importedUrl").val(" ");
+    });
+    $(".confirmUrl").click(function(){
+        $(".wrapperForImportUrlModal").removeClass("active");
+    });
+});
